403e6909e92fc7ea4ae6b50cdd26fe8446184da1
= <=>
2022-01-16 15:16:06 +0000

    Menu accordéon : ok

--- bulles/script.js
+++ bulles/script.js
@@ -1,6 +1,4 @@
 const btn = document.querySelector(".btn-center")
-console.log("hello");
-const list_btn = document.querySelectorAll(".btn-item")
 
 btn.addEventListener('click', () => {
     btn.classList.toggle("active")
@@ -8,5 +6,4 @@ btn.addEventListener('click', () => {
     for (let i = 1; i<9; i++){
         document.querySelector(`.item${i}`).classList.toggle("return")
     }
-    // document.querySelector(".item1").classList.toggle("return")
 })
